d2884c3814b4a82453927b902ed975ed36cad9f8
CzepiM200 <czepimm200@gmail.com>
2020-11-25 22:54:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/units_kotlin2/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.units_kotlin2
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.example.units_kotlin2.fragments.DistanceFragment
+import com.example.units_kotlin2.fragments.SurfaceFragment
+import com.example.units_kotlin2.fragments.VolumeFragment
+import com.example.units_kotlin2.ui.main.SectionsPagerAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        sectionsPagerAdapter.addFragment(DistanceFragment(), "Distance")
+        sectionsPagerAdapter.addFragment(SurfaceFragment(), "Surface")
+        sectionsPagerAdapter.addFragment(VolumeFragment(), "Volume")
+
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+        tabs.getTabAt(0)!!.setIcon(R.drawable.ic_baseline_all_inclusive_24)
+        tabs.getTabAt(1)!!.setIcon(R.drawable.ic_baseline_api_24)
+        tabs.getTabAt(2)!!.setIcon(R.drawable.ic_baseline_local_drink_24)
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+
+
+
+
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
